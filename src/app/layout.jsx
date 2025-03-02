2d593ce3a93e164d5019f3cf1198d16af004c0c3
vinalask3 <vinalask3@gmail.com>
2025-03-02 15:19:18 +0100

    Added styles and links

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+        <header>
+          <nav>
+            <Link href="/" className="nav-link">Home</Link>
+            <Link href="/dashboard" className="nav-link">Dashboard</Link>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>Footer</footer>
       </body>
     </html>
   );
