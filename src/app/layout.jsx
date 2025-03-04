5973f48aa8de812ed94d6a0489ed8f95da4639b6
vinalask3 <vinalask3@gmail.com>
2025-03-04 06:36:25 +0100

    Added Google fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import "./globals.css";
+import Poppins from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>
         <header>
           <nav>
